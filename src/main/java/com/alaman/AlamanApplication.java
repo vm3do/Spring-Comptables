46fe7fb0fa9ab42ef1f7745d09c524032ad45e22
vm3do <medoavvadi@gmail.com>
2025-11-17 09:00:00 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/alaman/AlamanApplication.java
@@ -0,0 +1,13 @@
+package com.alaman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlamanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlamanApplication.class, args);
+	}
+
+}
